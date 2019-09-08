02f707c51deb17797f51981e66c8af4011dcca79
Martin Mädler <martin.maedler@gmail.com>
2019-09-08 20:00:26 +0200

    Add lazy loaded route

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -4,7 +4,6 @@ import { CommonModule } from '@angular/common';
 import { AdminRoutingModule } from './admin-routing.module';
 import { AdminDashboardComponent } from '../admin-dashboard/admin-dashboard.component';
 
-
 @NgModule({
   declarations: [AdminDashboardComponent],
   imports: [
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AdminModule } from './admin/admin.module';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'admin',
+    loadChildren: () => import('./admin/admin.module').then(mod => mod.AdminModule)
+  },
+  {
+    path: '',
+    redirectTo: '',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
